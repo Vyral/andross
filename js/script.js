06ec4065a0e51f5d09c3d9e5946fd5e4d12dce59
Vyral  Marketing <getvyral@gmail.com>
2016-09-28 20:29:40 +0000

    Updated '/js/script.js' via CloudCannon

--- js/script.js
+++ js/script.js
@@ -70,7 +70,6 @@ $(document).ready(function() {
         wrap.style.width      = '100%';
         wrap.style.position   = 'relative';
         wrap.style.paddingTop = videoRatio + '%';
-        wrap.style.max-width   = '720px';
 
         /*
          * Add the iframe inside our newly created <div>
